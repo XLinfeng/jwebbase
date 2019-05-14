55e364fbc1922e431621fa8fbc444c75cdc8efc9
apple <xianglf1983@163.com>
2019-05-14 21:30:58 +0800

    项目初始化

---
+++ src/test/java/com/fanjia/jwebframe/JwebframeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fanjia.jwebframe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwebframeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
